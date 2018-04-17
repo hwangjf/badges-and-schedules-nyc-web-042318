d98e6a5c35042f69762f59a5dfc7a35dce20f6b1
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-17 23:40:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  array_badge_msgs = []
+  for i in array
+    array_badge_msgs.push(badge_maker(i))
+  end
+  return array_badge_msgs
+end
+
+def assign_rooms (speakers)
+  room_sign = []
+  for i in speakers
+    room_sign.push("Hello, #{i}! You'll be assigned to room " + i + "!")
+  end
+  return room_sign
+end
+
+def printer 
+  
+end
